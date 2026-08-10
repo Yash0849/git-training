67194482cf402e129554f3aab8e1367a03d983a2
Yash Vishwakarma <vishwakarma.yash0849@gmail.com>
2026-08-10 23:55:40 +0530

    3rd comit

---
+++ git03.py
@@ -0,0 +1,13 @@
+# Simple calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Cannot divide by zero.")
